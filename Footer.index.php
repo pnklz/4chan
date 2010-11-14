10ae9932682057a080b0ff03a082468ba9909b64
EpicDewd <EpicDewd@localhost>
2010-11-14 14:15:55 -0500

    CSS/PHP/HTML: Button CSS & Some new HTML/PHP

--- Footer.index.php
+++ Footer.index.php
@@ -1,44 +1,43 @@
-    <div id="sidebar" class="grid_4 omega">
-    
-        <aside class="widget">
-            <h3>Sidebar heading</h3>
-            
-            <ul>
-                <li><a href="#">Li element</a></li>
-                <li><a href="#">Li element</a></li>
-                <li><a href="#">Li element</a></li>
-                <li><a href="#">Li element</a></li>
-                <li><a href="#">Li element</a></li>
-            </ul>
-        </aside> <!-- end widget -->
-        
-        <aside class="widget">
-            <!-- Non working search -->
-            <form action="">
-                <input type="search" results="10" placeholder="Search..." />
-                <input type="submit" value="Search..." />
-            </form>
-        </aside> <!-- end widget -->
-        
-        <aside class="widget">
-            <h3>Widget</h3>
-            
-            <p>
-                Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.
-                One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. 
-            </p>
-        </aside> <!-- end widget -->
-    
-    </div> <!-- end sidebar -->
-    
-    </div> <!-- end content -->
-    
-    <footer id="footer" class="grid_12">
-        <!-- You're free to remove the credit link in the footer, but please, please leave it there. -->
-        <p>Copyright &copy; 2010 <a href="#">EpicDewd's 4chan</a></p>
-    </footer> <!-- end footer -->
-    
-    <div class="clear"></div>
+	<div id="sidebar" class="grid_4 omega">
+	
+		<aside class="widget">
+			<h3>Sidebar heading</h3>
+			
+			<ul>
+				<li><a href="#">Li element</a></li>
+				<li><a href="#">Li element</a></li>
+				<li><a href="#">Li element</a></li>
+				<li><a href="#">Li element</a></li>
+				<li><a href="#">Li element</a></li>
+			</ul>
+		</aside> <!-- end widget -->
+		
+		<aside class="widget">
+			<!-- Non working search -->
+			<form action="">
+				<input type="search" results="10" placeholder="Search..." />
+				<input type="submit" value="Search..." />
+			</form>
+		</aside> <!-- end widget -->
+		
+		<aside class="widget">
+			<h3>Widget</h3>
+			
+			<p>
+				Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.
+				One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. 
+			</p>
+		</aside> <!-- end widget -->
+	
+	</div> <!-- end sidebar -->
+	
+	</div> <!-- end content -->
+	
+	<footer id="footer" class="grid_12">
+		<p>Copyright &copy; 2010 <a href="#">EpicDewd's 4chan</a></p>
+	</footer> <!-- end footer -->
+	
+	<div class="clear"></div>
 
 </div> <!-- end wrapper -->
 
--- Header.index.php
+++ Header.index.php
@@ -2,57 +2,55 @@
 <html lang="en">
 <head>
 
-    <meta charset="utf-8" />
-    
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    
-    <title>4chan</title>
-    
-    <meta name="description" content="" />
-    
-    <link href="css/960.css" rel="stylesheet" type="text/css" />
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    
-    <!--[if lt IE 9]>
-    	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
-    <![endif]-->
-    
-    <!--[if lte IE 7]>
-    	<link href="css/ie.css" rel="stylesheet" type="text/css" />
-    <![endif]-->
+	<meta charset="utf-8" />
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	
+	<title>4chan</title>
+	
+	<meta name="description" content="" />
+	
+	<link href="css/960.css" rel="stylesheet" type="text/css" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+	<![endif]-->
+	
+	<!--[if lte IE 7]>
+		<link href="css/ie.css" rel="stylesheet" type="text/css" />
+	<![endif]-->
 
 </head>
 
 <body>
 	<div id="wrapper" class="container_12">
 
-	    <header id="header" class="grid_12">
+		<header id="header" class="grid_12">
 
-	    	<h1><a href="index.html">4chan</a></h1>
+			<h1><a href="index.html">4chan</a></h1>
 
-	    </header> <!-- end header -->
+		</header> <!-- end header -->
 
-	    <div id="content">
+		<div id="content">
 
-	        <nav>
-	            <ul id="menu" class="clearfix"> 
-	                <li class="current"><a href="index.php">Home</a></li>
-	                <li>
-	                	<a href="#">About</a>
-	                    <ul>
-	                        <li><a href="#">Subpage</a></li>
-	                        <li><a href="#">Subpage 2</a></li>
-	                    </ul>
-	                </li>
-	                <li><a href="#">Contact</a></li>
-	            </ul>
-	            <br class="clear" />
-	        </nav>
+			<nav>
+				<ul id="menu" class="clearfix"> 
+					<li class="current"><a href="index.php">Home</a></li>
+					<li>
+						<a href="#">About</a>
+						<ul>
+							<li><a href="#">Subpage</a></li>
+							<li><a href="#">Subpage 2</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<br class="clear" />
+			</nav>
 
-	        <!-- Show a "Please Upgrade" box to both IE7 and IE6 users (Edit to IE 6 if you just want to show it to IE6 users) - jQuery will load the content from js/ie.html into the div -->
-
-	        <!--[if lte IE 7]>
-	        	<div class="ie grid_7"></div>
-	        <![endif]-->
+			<!--[if lte IE 7]>
+				<div class="ie grid_7"></div>
+			<![endif]-->
 
 	
